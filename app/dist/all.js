bedcefc408f622dc27ba445f1daf78c8acb7023d
Anas ali <anas.cd.97@gmail.com>
2022-04-13 04:56:27 +0300

    styling finished
    the styling is ready to be used in Vue components, the styling well be kept in the scss directory seperate from the Vue components files for now

--- app/dist/all.js
+++ app/dist/all.js
@@ -1,2 +1,2 @@
-console.log("Load script.js");var app={};app.global={init:function(){console.log("load global functions"),app.global.loadHeader()},loadHeader:function(){console.log("loadHeader()")}},app.global.init();
+console.log("Load script.js");var app={};const el=document.querySelector("#commentor"),observer=new IntersectionObserver((([e])=>e.target.classList.toggle("stuck",e.intersectionRatio<1)),{threshold:[1]});observer.observe(el),app.global={init:function(){console.log("load global functions"),app.global.loadHeader()},loadHeader:function(){console.log("loadHeader()")},fetcher:function(e){}},app.global.init();const VueApp=Vue.createApp({data:function(){return{test:"heeeee"}}});
 //# sourceMappingURL=all.js.map
--- app/js/global/_global.js
+++ app/js/global/_global.js
@@ -1,4 +1,3 @@
-  
 // Global
 app.global = {
     init: function(){ // Load all global functions here
@@ -7,8 +6,23 @@ app.global = {
     },
     loadHeader: function(){ // Some specific function
         console.log("loadHeader()");
+    },
+    fetcher: function (filePath) {
+        
     }
-}
+};
+
+
 
 // Run the global stuff
 app.global.init();
+
+
+// Vue 
+const VueApp = Vue.createApp({
+    data: function() {
+        return {
+            test: 'heeeee'
+        }
+    }
+}); 
